26cb796cdc8cb66b0a3272928adc509d75e7eeef
displaseman <bardocost@gmail.com>
2021-08-14 02:48:23 +0300

    'test'

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function col(n){
 
 function setup() {
 
-  createCanvas(windowWidth, windowHeight)
+  createCanvas(, windowHeight)
 
   // noCursor() // Скрывает курсор
   frameRate(60) // количество кадров в секунду
